57f165b422422c61342b9b34dd7d41301aee4890
redhat <redhat.ua@gmail.com>
2017-01-04 15:10:20 +0200

    Added flags

--- src/scanner.go
+++ src/scanner.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net"
+	"strconv"
 	"strings"
 )
 
@@ -14,20 +16,49 @@ type ScanPortResult struct {
 }
 
 func main() {
-	var host = "127.0.0.1"
-	var ports = "22,80,443,8080"
+	var host = flag.String("h", "", "Target host")
+	var ports = flag.String("p", "", "Port(s) for scan")
+	flag.Parse()
 
-	if "" == host || "" == ports {
+	if "" == *host || "" == *ports {
 		log.Fatal("Die!")
 	}
 	results := []ScanPortResult{}
-	portSplit := strings.Split(ports, ",")
+	portSplit := parsePorts(*ports)
 	for _, port := range portSplit {
-		results = append(results, scanPort(host, port))
+		results = append(results, scanPort(*host, port))
 	}
 	for _, state := range results {
 		log.Printf("Port %s %s", state.Port, map[bool]string{true: "open", false: "closed"}[state.State])
 	}
+}
+
+func parsePorts(ports string) []string {
+	var result = []string{}
+	split := strings.Split(ports, ",")
+	for _, port := range split {
+		if strings.Contains(port, "-") {
+			if r, err := splitRange(port); err == nil {
+				result = append(result, r...)
+			}
+		} else {
+			result = append(result, port)
+		}
+	}
+
+	return result
+
+}
+
+func splitRange(r string) ([]string, error) {
+	result := []string{}
+	split := strings.SplitN(r, "-", 2)
+	min, _ := strconv.Atoi(split[0])
+	max, _ := strconv.Atoi(split[1])
+	for i := min; i <= max; i++ {
+		result = append(result, strconv.Itoa(i))
+	}
+	return result, nil
 }
 
 func scanPort(h, p string) ScanPortResult {
